99e8cb7e80426ea84584260a7e886f3ea70e051d
Tyr Chen <tyr.chen@gmail.com>
2019-01-22 20:40:53 -0800

    reorder vendor proto so that the dependency is clear.

--- lib/abci_protos/vendor.pb.ex
+++ lib/abci_protos/vendor.pb.ex
@@ -40,67 +40,249 @@ defmodule ForgeVendor.Proof do
   field :ops, 1, repeated: true, type: ForgeVendor.ProofOp
 end
 
-defmodule ForgeVendor.RequestPing do
-  @moduledoc false
-  use Protobuf, syntax: :proto3
-
-  defstruct []
-end
-
-defmodule ForgeVendor.RequestBroadcastTx do
+defmodule ForgeVendor.BlockSizeParams do
   @moduledoc false
   use Protobuf, syntax: :proto3
 
   @type t :: %__MODULE__{
-          tx: String.t()
+          max_bytes: integer,
+          max_gas: integer
         }
-  defstruct [:tx]
+  defstruct [:max_bytes, :max_gas]
 
-  field :tx, 1, type: :bytes
+  field :max_bytes, 1, type: :int64
+  field :max_gas, 2, type: :int64
 end
 
-defmodule ForgeVendor.ResponsePing do
-  @moduledoc false
-  use Protobuf, syntax: :proto3
-
-  defstruct []
-end
-
-defmodule ForgeVendor.ResponseBroadcastTx do
+defmodule ForgeVendor.EvidenceParams do
   @moduledoc false
   use Protobuf, syntax: :proto3
 
   @type t :: %__MODULE__{
-          check_tx: ForgeVendor.ResponseCheckTx.t(),
-          deliver_tx: ForgeVendor.ResponseDeliverTx.t()
+          max_age: integer
         }
-  defstruct [:check_tx, :deliver_tx]
+  defstruct [:max_age]
 
-  field :check_tx, 1, type: ForgeVendor.ResponseCheckTx
-  field :deliver_tx, 2, type: ForgeVendor.ResponseDeliverTx
+  field :max_age, 1, type: :int64
 end
 
-defmodule ForgeVendor.Request do
+defmodule ForgeVendor.ValidatorParams do
   @moduledoc false
   use Protobuf, syntax: :proto3
 
   @type t :: %__MODULE__{
-          value: {atom, any}
+          pub_key_types: [String.t()]
         }
-  defstruct [:value]
+  defstruct [:pub_key_types]
 
-  oneof :value, 0
-  field :echo, 2, type: ForgeVendor.RequestEcho, oneof: 0
-  field :flush, 3, type: ForgeVendor.RequestFlush, oneof: 0
-  field :info, 4, type: ForgeVendor.RequestInfo, oneof: 0
-  field :set_option, 5, type: ForgeVendor.RequestSetOption, oneof: 0
-  field :init_chain, 6, type: ForgeVendor.RequestInitChain, oneof: 0
-  field :query, 7, type: ForgeVendor.RequestQuery, oneof: 0
-  field :begin_block, 8, type: ForgeVendor.RequestBeginBlock, oneof: 0
-  field :check_tx, 9, type: ForgeVendor.RequestCheckTx, oneof: 0
-  field :deliver_tx, 19, type: ForgeVendor.RequestDeliverTx, oneof: 0
-  field :end_block, 11, type: ForgeVendor.RequestEndBlock, oneof: 0
-  field :commit, 12, type: ForgeVendor.RequestCommit, oneof: 0
+  field :pub_key_types, 1, repeated: true, type: :string
+end
+
+defmodule ForgeVendor.ConsensusParams do
+  @moduledoc false
+  use Protobuf, syntax: :proto3
+
+  @type t :: %__MODULE__{
+          block_size: ForgeVendor.BlockSizeParams.t(),
+          evidence: ForgeVendor.EvidenceParams.t(),
+          validator: ForgeVendor.ValidatorParams.t()
+        }
+  defstruct [:block_size, :evidence, :validator]
+
+  field :block_size, 1, type: ForgeVendor.BlockSizeParams
+  field :evidence, 2, type: ForgeVendor.EvidenceParams
+  field :validator, 3, type: ForgeVendor.ValidatorParams
+end
+
+defmodule ForgeVendor.LastCommitInfo do
+  @moduledoc false
+  use Protobuf, syntax: :proto3
+
+  @type t :: %__MODULE__{
+          round: integer,
+          votes: [ForgeVendor.VoteInfo.t()]
+        }
+  defstruct [:round, :votes]
+
+  field :round, 1, type: :int32
+  field :votes, 2, repeated: true, type: ForgeVendor.VoteInfo
+end
+
+defmodule ForgeVendor.Version do
+  @moduledoc false
+  use Protobuf, syntax: :proto3
+
+  @type t :: %__MODULE__{
+          Block: non_neg_integer,
+          App: non_neg_integer
+        }
+  defstruct [:Block, :App]
+
+  field :Block, 1, type: :uint64
+  field :App, 2, type: :uint64
+end
+
+defmodule ForgeVendor.PartSetHeader do
+  @moduledoc false
+  use Protobuf, syntax: :proto3
+
+  @type t :: %__MODULE__{
+          total: integer,
+          hash: String.t()
+        }
+  defstruct [:total, :hash]
+
+  field :total, 1, type: :int32
+  field :hash, 2, type: :bytes
+end
+
+defmodule ForgeVendor.BlockID do
+  @moduledoc false
+  use Protobuf, syntax: :proto3
+
+  @type t :: %__MODULE__{
+          hash: String.t(),
+          parts_header: ForgeVendor.PartSetHeader.t()
+        }
+  defstruct [:hash, :parts_header]
+
+  field :hash, 1, type: :bytes
+  field :parts_header, 2, type: ForgeVendor.PartSetHeader
+end
+
+defmodule ForgeVendor.Validator do
+  @moduledoc false
+  use Protobuf, syntax: :proto3
+
+  @type t :: %__MODULE__{
+          address: String.t(),
+          power: integer
+        }
+  defstruct [:address, :power]
+
+  field :address, 1, type: :bytes
+  field :power, 3, type: :int64
+end
+
+defmodule ForgeVendor.PubKey do
+  @moduledoc false
+  use Protobuf, syntax: :proto3
+
+  @type t :: %__MODULE__{
+          type: String.t(),
+          data: String.t()
+        }
+  defstruct [:type, :data]
+
+  field :type, 1, type: :string
+  field :data, 2, type: :bytes
+end
+
+defmodule ForgeVendor.ValidatorUpdate do
+  @moduledoc false
+  use Protobuf, syntax: :proto3
+
+  @type t :: %__MODULE__{
+          pub_key: ForgeVendor.PubKey.t(),
+          power: integer
+        }
+  defstruct [:pub_key, :power]
+
+  field :pub_key, 1, type: ForgeVendor.PubKey
+  field :power, 2, type: :int64
+end
+
+defmodule ForgeVendor.VoteInfo do
+  @moduledoc false
+  use Protobuf, syntax: :proto3
+
+  @type t :: %__MODULE__{
+          validator: ForgeVendor.Validator.t(),
+          signed_last_block: boolean
+        }
+  defstruct [:validator, :signed_last_block]
+
+  field :validator, 1, type: ForgeVendor.Validator
+  field :signed_last_block, 2, type: :bool
+end
+
+defmodule ForgeVendor.Evidence do
+  @moduledoc false
+  use Protobuf, syntax: :proto3
+
+  @type t :: %__MODULE__{
+          type: String.t(),
+          validator: ForgeVendor.Validator.t(),
+          height: integer,
+          time: Google.Protobuf.Timestamp.t(),
+          total_voting_power: integer
+        }
+  defstruct [:type, :validator, :height, :time, :total_voting_power]
+
+  field :type, 1, type: :string
+  field :validator, 2, type: ForgeVendor.Validator
+  field :height, 3, type: :int64
+  field :time, 4, type: Google.Protobuf.Timestamp
+  field :total_voting_power, 5, type: :int64
+end
+
+defmodule ForgeVendor.Header do
+  @moduledoc false
+  use Protobuf, syntax: :proto3
+
+  @type t :: %__MODULE__{
+          version: ForgeVendor.Version.t(),
+          chain_id: String.t(),
+          height: integer,
+          time: Google.Protobuf.Timestamp.t(),
+          num_txs: integer,
+          total_txs: integer,
+          last_block_id: ForgeVendor.BlockID.t(),
+          last_commit_hash: String.t(),
+          data_hash: String.t(),
+          validators_hash: String.t(),
+          next_validators_hash: String.t(),
+          consensus_hash: String.t(),
+          app_hash: String.t(),
+          last_results_hash: String.t(),
+          evidence_hash: String.t(),
+          proposer_address: String.t()
+        }
+  defstruct [
+    :version,
+    :chain_id,
+    :height,
+    :time,
+    :num_txs,
+    :total_txs,
+    :last_block_id,
+    :last_commit_hash,
+    :data_hash,
+    :validators_hash,
+    :next_validators_hash,
+    :consensus_hash,
+    :app_hash,
+    :last_results_hash,
+    :evidence_hash,
+    :proposer_address
+  ]
+
+  field :version, 1, type: ForgeVendor.Version
+  field :chain_id, 2, type: :string
+  field :height, 3, type: :int64
+  field :time, 4, type: Google.Protobuf.Timestamp
+  field :num_txs, 5, type: :int64
+  field :total_txs, 6, type: :int64
+  field :last_block_id, 7, type: ForgeVendor.BlockID
+  field :last_commit_hash, 8, type: :bytes
+  field :data_hash, 9, type: :bytes
+  field :validators_hash, 10, type: :bytes
+  field :next_validators_hash, 11, type: :bytes
+  field :consensus_hash, 12, type: :bytes
+  field :app_hash, 13, type: :bytes
+  field :last_results_hash, 14, type: :bytes
+  field :evidence_hash, 15, type: :bytes
+  field :proposer_address, 16, type: :bytes
 end
 
 defmodule ForgeVendor.RequestEcho do
@@ -251,7 +433,7 @@ defmodule ForgeVendor.RequestCommit do
   defstruct []
 end
 
-defmodule ForgeVendor.Response do
+defmodule ForgeVendor.Request do
   @moduledoc false
   use Protobuf, syntax: :proto3
 
@@ -261,18 +443,17 @@ defmodule ForgeVendor.Response do
   defstruct [:value]
 
   oneof :value, 0
-  field :exception, 1, type: ForgeVendor.ResponseException, oneof: 0
-  field :echo, 2, type: ForgeVendor.ResponseEcho, oneof: 0
-  field :flush, 3, type: ForgeVendor.ResponseFlush, oneof: 0
-  field :info, 4, type: ForgeVendor.ResponseInfo, oneof: 0
-  field :set_option, 5, type: ForgeVendor.ResponseSetOption, oneof: 0
-  field :init_chain, 6, type: ForgeVendor.ResponseInitChain, oneof: 0
-  field :query, 7, type: ForgeVendor.ResponseQuery, oneof: 0
-  field :begin_block, 8, type: ForgeVendor.ResponseBeginBlock, oneof: 0
-  field :check_tx, 9, type: ForgeVendor.ResponseCheckTx, oneof: 0
-  field :deliver_tx, 10, type: ForgeVendor.ResponseDeliverTx, oneof: 0
-  field :end_block, 11, type: ForgeVendor.ResponseEndBlock, oneof: 0
-  field :commit, 12, type: ForgeVendor.ResponseCommit, oneof: 0
+  field :echo, 2, type: ForgeVendor.RequestEcho, oneof: 0
+  field :flush, 3, type: ForgeVendor.RequestFlush, oneof: 0
+  field :info, 4, type: ForgeVendor.RequestInfo, oneof: 0
+  field :set_option, 5, type: ForgeVendor.RequestSetOption, oneof: 0
+  field :init_chain, 6, type: ForgeVendor.RequestInitChain, oneof: 0
+  field :query, 7, type: ForgeVendor.RequestQuery, oneof: 0
+  field :begin_block, 8, type: ForgeVendor.RequestBeginBlock, oneof: 0
+  field :check_tx, 9, type: ForgeVendor.RequestCheckTx, oneof: 0
+  field :deliver_tx, 19, type: ForgeVendor.RequestDeliverTx, oneof: 0
+  field :end_block, 11, type: ForgeVendor.RequestEndBlock, oneof: 0
+  field :commit, 12, type: ForgeVendor.RequestCommit, oneof: 0
 end
 
 defmodule ForgeVendor.ResponseException do
@@ -476,249 +657,68 @@ defmodule ForgeVendor.ResponseCommit do
   field :data, 2, type: :bytes
 end
 
-defmodule ForgeVendor.ConsensusParams do
+defmodule ForgeVendor.Response do
   @moduledoc false
   use Protobuf, syntax: :proto3
 
   @type t :: %__MODULE__{
-          block_size: ForgeVendor.BlockSizeParams.t(),
-          evidence: ForgeVendor.EvidenceParams.t(),
-          validator: ForgeVendor.ValidatorParams.t()
+          value: {atom, any}
         }
-  defstruct [:block_size, :evidence, :validator]
+  defstruct [:value]
 
-  field :block_size, 1, type: ForgeVendor.BlockSizeParams
-  field :evidence, 2, type: ForgeVendor.EvidenceParams
-  field :validator, 3, type: ForgeVendor.ValidatorParams
+  oneof :value, 0
+  field :exception, 1, type: ForgeVendor.ResponseException, oneof: 0
+  field :echo, 2, type: ForgeVendor.ResponseEcho, oneof: 0
+  field :flush, 3, type: ForgeVendor.ResponseFlush, oneof: 0
+  field :info, 4, type: ForgeVendor.ResponseInfo, oneof: 0
+  field :set_option, 5, type: ForgeVendor.ResponseSetOption, oneof: 0
+  field :init_chain, 6, type: ForgeVendor.ResponseInitChain, oneof: 0
+  field :query, 7, type: ForgeVendor.ResponseQuery, oneof: 0
+  field :begin_block, 8, type: ForgeVendor.ResponseBeginBlock, oneof: 0
+  field :check_tx, 9, type: ForgeVendor.ResponseCheckTx, oneof: 0
+  field :deliver_tx, 10, type: ForgeVendor.ResponseDeliverTx, oneof: 0
+  field :end_block, 11, type: ForgeVendor.ResponseEndBlock, oneof: 0
+  field :commit, 12, type: ForgeVendor.ResponseCommit, oneof: 0
 end
 
-defmodule ForgeVendor.BlockSizeParams do
+defmodule ForgeVendor.RequestPing do
   @moduledoc false
   use Protobuf, syntax: :proto3
 
-  @type t :: %__MODULE__{
-          max_bytes: integer,
-          max_gas: integer
-        }
-  defstruct [:max_bytes, :max_gas]
-
-  field :max_bytes, 1, type: :int64
-  field :max_gas, 2, type: :int64
+  defstruct []
 end
 
-defmodule ForgeVendor.EvidenceParams do
+defmodule ForgeVendor.RequestBroadcastTx do
   @moduledoc false
   use Protobuf, syntax: :proto3
 
   @type t :: %__MODULE__{
-          max_age: integer
+          tx: String.t()
         }
-  defstruct [:max_age]
+  defstruct [:tx]
 
-  field :max_age, 1, type: :int64
+  field :tx, 1, type: :bytes
 end
 
-defmodule ForgeVendor.ValidatorParams do
+defmodule ForgeVendor.ResponsePing do
   @moduledoc false
   use Protobuf, syntax: :proto3
 
-  @type t :: %__MODULE__{
-          pub_key_types: [String.t()]
-        }
-  defstruct [:pub_key_types]
-
-  field :pub_key_types, 1, repeated: true, type: :string
+  defstruct []
 end
 
-defmodule ForgeVendor.LastCommitInfo do
+defmodule ForgeVendor.ResponseBroadcastTx do
   @moduledoc false
   use Protobuf, syntax: :proto3
 
   @type t :: %__MODULE__{
-          round: integer,
-          votes: [ForgeVendor.VoteInfo.t()]
+          check_tx: ForgeVendor.ResponseCheckTx.t(),
+          deliver_tx: ForgeVendor.ResponseDeliverTx.t()
         }
-  defstruct [:round, :votes]
+  defstruct [:check_tx, :deliver_tx]
 
-  field :round, 1, type: :int32
-  field :votes, 2, repeated: true, type: ForgeVendor.VoteInfo
-end
-
-defmodule ForgeVendor.Header do
-  @moduledoc false
-  use Protobuf, syntax: :proto3
-
-  @type t :: %__MODULE__{
-          version: ForgeVendor.Version.t(),
-          chain_id: String.t(),
-          height: integer,
-          time: Google.Protobuf.Timestamp.t(),
-          num_txs: integer,
-          total_txs: integer,
-          last_block_id: ForgeVendor.BlockID.t(),
-          last_commit_hash: String.t(),
-          data_hash: String.t(),
-          validators_hash: String.t(),
-          next_validators_hash: String.t(),
-          consensus_hash: String.t(),
-          app_hash: String.t(),
-          last_results_hash: String.t(),
-          evidence_hash: String.t(),
-          proposer_address: String.t()
-        }
-  defstruct [
-    :version,
-    :chain_id,
-    :height,
-    :time,
-    :num_txs,
-    :total_txs,
-    :last_block_id,
-    :last_commit_hash,
-    :data_hash,
-    :validators_hash,
-    :next_validators_hash,
-    :consensus_hash,
-    :app_hash,
-    :last_results_hash,
-    :evidence_hash,
-    :proposer_address
-  ]
-
-  field :version, 1, type: ForgeVendor.Version
-  field :chain_id, 2, type: :string
-  field :height, 3, type: :int64
-  field :time, 4, type: Google.Protobuf.Timestamp
-  field :num_txs, 5, type: :int64
-  field :total_txs, 6, type: :int64
-  field :last_block_id, 7, type: ForgeVendor.BlockID
-  field :last_commit_hash, 8, type: :bytes
-  field :data_hash, 9, type: :bytes
-  field :validators_hash, 10, type: :bytes
-  field :next_validators_hash, 11, type: :bytes
-  field :consensus_hash, 12, type: :bytes
-  field :app_hash, 13, type: :bytes
-  field :last_results_hash, 14, type: :bytes
-  field :evidence_hash, 15, type: :bytes
-  field :proposer_address, 16, type: :bytes
-end
-
-defmodule ForgeVendor.Version do
-  @moduledoc false
-  use Protobuf, syntax: :proto3
-
-  @type t :: %__MODULE__{
-          Block: non_neg_integer,
-          App: non_neg_integer
-        }
-  defstruct [:Block, :App]
-
-  field :Block, 1, type: :uint64
-  field :App, 2, type: :uint64
-end
-
-defmodule ForgeVendor.BlockID do
-  @moduledoc false
-  use Protobuf, syntax: :proto3
-
-  @type t :: %__MODULE__{
-          hash: String.t(),
-          parts_header: ForgeVendor.PartSetHeader.t()
-        }
-  defstruct [:hash, :parts_header]
-
-  field :hash, 1, type: :bytes
-  field :parts_header, 2, type: ForgeVendor.PartSetHeader
-end
-
-defmodule ForgeVendor.PartSetHeader do
-  @moduledoc false
-  use Protobuf, syntax: :proto3
-
-  @type t :: %__MODULE__{
-          total: integer,
-          hash: String.t()
-        }
-  defstruct [:total, :hash]
-
-  field :total, 1, type: :int32
-  field :hash, 2, type: :bytes
-end
-
-defmodule ForgeVendor.Validator do
-  @moduledoc false
-  use Protobuf, syntax: :proto3
-
-  @type t :: %__MODULE__{
-          address: String.t(),
-          power: integer
-        }
-  defstruct [:address, :power]
-
-  field :address, 1, type: :bytes
-  field :power, 3, type: :int64
-end
-
-defmodule ForgeVendor.ValidatorUpdate do
-  @moduledoc false
-  use Protobuf, syntax: :proto3
-
-  @type t :: %__MODULE__{
-          pub_key: ForgeVendor.PubKey.t(),
-          power: integer
-        }
-  defstruct [:pub_key, :power]
-
-  field :pub_key, 1, type: ForgeVendor.PubKey
-  field :power, 2, type: :int64
-end
-
-defmodule ForgeVendor.VoteInfo do
-  @moduledoc false
-  use Protobuf, syntax: :proto3
-
-  @type t :: %__MODULE__{
-          validator: ForgeVendor.Validator.t(),
-          signed_last_block: boolean
-        }
-  defstruct [:validator, :signed_last_block]
-
-  field :validator, 1, type: ForgeVendor.Validator
-  field :signed_last_block, 2, type: :bool
-end
-
-defmodule ForgeVendor.PubKey do
-  @moduledoc false
-  use Protobuf, syntax: :proto3
-
-  @type t :: %__MODULE__{
-          type: String.t(),
-          data: String.t()
-        }
-  defstruct [:type, :data]
-
-  field :type, 1, type: :string
-  field :data, 2, type: :bytes
-end
-
-defmodule ForgeVendor.Evidence do
-  @moduledoc false
-  use Protobuf, syntax: :proto3
-
-  @type t :: %__MODULE__{
-          type: String.t(),
-          validator: ForgeVendor.Validator.t(),
-          height: integer,
-          time: Google.Protobuf.Timestamp.t(),
-          total_voting_power: integer
-        }
-  defstruct [:type, :validator, :height, :time, :total_voting_power]
-
-  field :type, 1, type: :string
-  field :validator, 2, type: ForgeVendor.Validator
-  field :height, 3, type: :int64
-  field :time, 4, type: Google.Protobuf.Timestamp
-  field :total_voting_power, 5, type: :int64
+  field :check_tx, 1, type: ForgeVendor.ResponseCheckTx
+  field :deliver_tx, 2, type: ForgeVendor.ResponseDeliverTx
 end
 
 defmodule ForgeVendor.ABCIApplication.Service do
